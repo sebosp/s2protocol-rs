1eb8b89fb6315bf007b51ea420529034da21dab7
Seb Ospina <kraige@gmail.com>
2023-05-06 18:25:03 +0200

    Init handle unit born on s2-protocol side
    Signed-off-by: Seb Ospina <kraige@gmail.com>

--- src/tracker_events/state.rs
+++ src/tracker_events/state.rs
@@ -3,6 +3,7 @@
 use super::*;
 use crate::*;
 
+/// Handles the state management for unit init messages
 #[tracing::instrument(level = "debug", skip(sc2_replay))]
 pub fn handle_unit_init(
     sc2_replay: &mut SC2ReplayState,
@@ -30,4 +31,35 @@ pub fn handle_unit_init(
     }
     sc2_replay.units.insert(unit_init.unit_tag_index, sc2_unit);
     Some(unit_init.unit_tag_index)
+}
+
+/// Handles the state management for unit born messages
+#[tracing::instrument(level = "debug", skip(sc2_replay))]
+pub fn handle_unit_born(
+    sc2_replay: &mut SC2ReplayState,
+    game_loop: i64,
+    unit_born: &UnitBornEvent,
+) -> Option<u32> {
+    let unit_name_filter = &sc2_replay.filters.unit_name;
+    if let Some(unit_name_filter) = unit_name_filter {
+        if unit_name_filter != &unit_born.unit_type_name {
+            return None;
+        }
+    }
+    if let Some(ref mut sc2_unit) = sc2_replay.units.get_mut(&unit_born.unit_tag_index) {
+        sc2_unit.creator_ability_name = unit_born.creator_ability_name.clone();
+        sc2_unit.last_game_loop = game_loop;
+    } else {
+        let sc2_unit = SC2Unit {
+            last_game_loop: game_loop,
+            user_id: Some(unit_born.control_player_id),
+            name: unit_born.unit_type_name.clone(),
+            pos: Vec3D::new(unit_born.x as f32, -1. * unit_born.y as f32, 0.),
+            init_game_loop: game_loop,
+            radius: unit_size,
+            ..Default::default()
+        };
+        sc2_replay.units.insert(unit_born.unit_tag_index, sc2_unit);
+    }
+    Ok(())
 }
